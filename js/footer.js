b591e0b7429496fc43827d5f0be5264b94e2b1b6
Rahul Katari <rahulkatari99@gmail.com>
2025-05-10 20:07:14 +0530

    footer changes

--- js/footer.js
+++ js/footer.js
@@ -1,13 +1,7 @@
 const footerHTML = `<div class="max-w-7xl container md:px-5 bg-dark text-light footer md:pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s">
         <div class=" py-5">
-            <div class="row g-5">
-                <div class="col-lg-3 col-md-6 d-none">
-                    <h4 class="text-white mb-3">Quick Link</h4>
-                    <a class="btn btn-link" href="advanced-sap-fico-training-hyderabad.html">About Us</a>
-                    <a class="btn btn-link" href="courses.html">Courses</a>
-                    <a class="btn btn-link" href="best-sap-training-institutes-with-placements-contactus.html">Contact Us</a>
-                </div>
-                <div class="col-lg-4 col-md-6">
+            <div class="md:grid grid-cols-3 g-5 space-y-5 md:space-y-0 gap-5">
+                <div>
                     <div class="shadow rounded-xl bg-gradient p-4 h-full flex flex-col justify-between">
                     <h1 class="text-white mb-3 flex items-center md:text-4xl text-2xl">USA <img src="./img/footer/usa.jpg" class="ms-5 h-[100px] w-[100px] object-cover rounded-full"></h1>
                     <div class="">
@@ -27,7 +21,7 @@ const footerHTML = `<div class="max-w-7xl container md:px-5 bg-dark text-light f
                     </div></div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6">
+                <div>
                     <div class="shadow rounded-xl bg-gradient p-4 h-full flex flex-col justify-between">
                         <h1 class="text-white mb-3 flex items-center md:text-4xl text-2xl">Hyderabad <img src="./img/footer/hyderabad.png" class="ms-5 h-[100px] w-[100px] object-cover rounded-full"></h1>
                         <div>
@@ -47,7 +41,7 @@ const footerHTML = `<div class="max-w-7xl container md:px-5 bg-dark text-light f
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6">
+                <div>
                     <div class="shadow rounded-xl bg-gradient p-4">
                         <h1 class="text-white mb-3 flex items-center md:text-4xl text-2xl">Bengaluru<img src="./img/footer/bengaluru.png" class="ms-5 h-[100px] w-[100px] object-cover rounded-full"></h1>
                         <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>No.12,165/1, Masjid Building, 1st Floor, Begur Main Road, Bommanahalli, Bengaluru - 560068</p>
@@ -63,38 +57,6 @@ const footerHTML = `<div class="max-w-7xl container md:px-5 bg-dark text-light f
                             <a class="btn btn-outline-light btn-social" href="https://www.youtube.com/@sapficohub"><i class="fab fa-youtube"></i></a>
                             <a class="btn btn-outline-light btn-social" href="#"><i class="fab fa-linkedin-in"></i></a>
                         </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 d-none">
-                    <h4 class="text-white mb-3">Gallery</h4>
-                    <div class="row g-2 pt-2">
-                        <div class="col-4">
-                            <img class="img-fluid bg-light p-1" src="img/course-1.jpg" alt="">
-                        </div>
-                        <div class="col-4">
-                            <img class="img-fluid bg-light p-1" src="img/course-2.jpg" alt="">
-                        </div>
-                        <div class="col-4">
-                            <img class="img-fluid bg-light p-1" src="img/course-3.jpg" alt="">
-                        </div>
-                        <div class="col-4">
-                            <img class="img-fluid bg-light p-1" src="img/course-2.jpg" alt="">
-                        </div>
-                        <div class="col-4">
-                            <img class="img-fluid bg-light p-1" src="img/course-3.jpg" alt="">
-                        </div>
-                        <div class="col-4">
-                            <img class="img-fluid bg-light p-1" src="img/course-1.jpg" alt="">
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 d-none">
-                    <h4 class="text-white mb-3">Newsletter</h4>
-                    <p>Subscribe to the MyERP HUB Newsletter</p>
-                    <div class="position-relative mx-auto" style="max-width: 400px;">
-                        <input class="form-control border-0 w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email">
-                        <button type="button"
-                            class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
                     </div>
                 </div>
             </div>
